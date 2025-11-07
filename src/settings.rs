292a3c1d96b61ad42745c5be465128579e8c249a
Gilles Vink <gilles@vinkvfx.com>
2025-11-07 17:26:13 +0100

    Add cosntructor for settings

--- src/settings.rs
+++ src/settings.rs
@@ -14,7 +14,9 @@ pub struct Filmback {
     pub y: f32,
 }
 
+#[cfg_attr(feature = "wasm-bindings", wasm_bindgen)]
 impl Filmback {
+    #[cfg_attr(feature = "wasm-bindings", wasm_bindgen(constructor))]
     pub fn new(x: f32, y: f32) -> Self {
         Self { x, y }
     }
@@ -29,7 +31,9 @@ pub struct Resolution {
     pub y: u32,
 }
 
+#[cfg_attr(feature = "wasm-bindings", wasm_bindgen)]
 impl Resolution {
+    #[cfg_attr(feature = "wasm-bindings", wasm_bindgen(constructor))]
     pub fn new(x: u32, y: u32) -> Self {
         Self { x, y }
     }
